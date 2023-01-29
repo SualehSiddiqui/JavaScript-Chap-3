bd1850be1384254138a4b5852c79bf0fc889a0a6
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-01-29 21:00:48 +0500

    adding js assignment 2 chap 3
    adding js assignment 2 chap 3

---
+++ app.js
@@ -0,0 +1,23 @@
+// Chap #3
+
+// Question#1
+// var age = ("16 years old");
+
+// window.alert("I am "+age);
+
+// Question#2
+// var num = (1);
+
+// window.alert ("You have visited this site "+num+" Times");
+
+// Question#3
+// var birthYear = (2006);
+
+// document.write("My birth year is " +birthYear+"<br /> Data type my decleared variable is Number");
+
+// Question#4
+// var name = ("M.Sualeh");
+// var product = ("Jacket(s)");
+// var quan = (2);
+
+// document.write('“<b>'+name+'</b> ordered <b>'+quan+ product+'</b> on XYZ Clothing store”.');
